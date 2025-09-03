8730174834bbde0017c99a42163468494d63174f
dps3007 <deepanshupal30072002@gmail.com>
2025-09-04 02:42:43 +0530

    add notes comment

---
+++ backend/models/ProjectNoteComment.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectNoteCommentSchema = new Schema(
+  {
+    note: {
+      type: Schema.Types.ObjectId,
+      ref: "ProjectNote",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    parentComment: {
+      type: Schema.Types.ObjectId,
+      ref: "ProjectNoteComment", // for threaded replies
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ProjectNoteComment = mongoose.model(
+  "ProjectNoteComment",
+  projectNoteCommentSchema
+);
